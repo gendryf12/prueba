8263e4dbb2e9c52e4e9df2d4987c5c19ced522b5
soa-a208-e-004 <soa-a208-e-004@soa-a208-e-004.UMD.LOCAL.EDU>
2019-10-22 21:34:14 -0500

    cambio de claves

--- db/db.php
+++ db/db.php
@@ -1,7 +1,7 @@
 <?php
 class Conectar{
     public static function conexion(){
-        $conexion=new mysqli("remotemysql.com", "7tK58L6A24", "4iULrSz7Xe", "7tK58L6A24");
+        $conexion=new mysqli("remotemysql.com", "EpkE47OQAk", "EpkE47OQAk", "krTtukOMYb");
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
